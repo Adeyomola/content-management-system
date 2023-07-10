fe9c6dd45c9b4023961bd7be771e69bc169c5302
Ubuntu <ubuntu@ip-172-31-31-152.eu-west-1.compute.internal>
2023-07-10 12:52:53 +0000

    removed some files

--- provision/providers.tf
+++ provision/providers.tf
@@ -16,7 +16,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.10.1"
     }
-
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.21.1"
+    }
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0.4"
